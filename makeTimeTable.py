67147bee1d18d79b7fcc6fe08b46a36746fa19ce
Raymond Martinez <raymondmartinez@Raymonds-MBP.home>
2020-08-22 16:35:38 -0400

    Modify functionality added

--- makeTimeTable.py
+++ makeTimeTable.py
@@ -12,7 +12,7 @@ class TimeTableMachine():
         if choice == "1":
             self.createTable()
         elif choice == "2":
-            print("Feature coming soon in Version 2")
+            self.modTimeTable()
             show = Options()
             self.optionProcessor()
         elif choice == "3":
@@ -43,6 +43,20 @@ class TimeTableMachine():
     
     def showTable(self):
         print(self.timeTable)
+        show = Options()
+        self.optionProcessor()
+    
+    def modTimeTable(self):
+        entry = input("Give me an entry number, please")
+        while entry not in self.timeTable:
+            print("Error: Entry not found. Try again")
+            entry = input("")
+        company = input("Rail Company: ")
+        trainNum = input("Train Number: ")
+        arrTime = input("Expected Arrival: ")
+        dest = input("Destination: ")
+        trainString = f"Station: {self.station}, Company: {company}, #{trainNum}, Due: {arrTime}, Destination: {dest}"
+        self.timeTable[entry] = trainString
         show = Options()
         self.optionProcessor()
 
