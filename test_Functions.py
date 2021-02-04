24311e1e0abd74218c77cb93dac096f526379326
Raymond Martinez <raymondmartinez@Raymonds-MBP.home>
2021-02-03 22:05:42 -0500

    Replaced pytest with unittest

--- test_Functions.py
+++ test_Functions.py
@@ -1,29 +1,33 @@
 import sqlite3
 import json
+import unittest
 # import sys
+class BasicTestCase(unittest.TestCase):
+    def test_sqlite(self):
+        sqliteConnection = sqlite3.connect('Stations.sqlite3')
+        cursor = sqliteConnection.cursor()
+        cursor.execute("select * from Stations")
+        stations_L = cursor.fetchall()
+        stations = dict(stations_L)
+        print("PASS")
 
-def test_sqlite():
-    sqliteConnection = sqlite3.connect('Stations.sqlite3')
-    cursor = sqliteConnection.cursor()
-    cursor.execute("select * from Stations")
-    stations_L = cursor.fetchall()
-    stations = dict(stations_L)
-    print("PASS")
-
-def test_json():
-    timeTable = {}
-    station = "Stamford"
-    entry = "1"
-    company = "Amtrak"
-    trainNum = "66"
-    arrTime = "3:30 AM"
-    dest = "Boston, MA"
-    trainString = f"Company: {company}, #{trainNum}, Due: {arrTime}, Destination: {dest}"
-    timeTable[entry] = trainString
-    if timeTable == {}:
-        print("FAIL")
+    def test_json(self):
+        timeTable = {}
+        station = "Stamford"
+        entry = "1"
+        company = "Amtrak"
+        trainNum = "66"
+        arrTime = "3:30 AM"
+        dest = "Boston, MA"
+        trainString = f"Company: {company}, #{trainNum}, Due: {arrTime}, Destination: {dest}"
+        timeTable[entry] = trainString
+        if timeTable == {}:
+            print("FAIL")
     
-    else:
-        with open(f'{station}.json', 'w') as w:
-            json.dump(timeTable, w, sort_keys=True, indent = 3)
+        else:
+            with open(f'{station}.json', 'w') as w:
+                json.dump(timeTable, w, sort_keys=True, indent = 3)
+
+if __name__ == '__main__':
+    unittest.main()
     
